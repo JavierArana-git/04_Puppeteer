e12d2e34d6d6f2637a3476b20fbd7aff2f52d5e1
JavierArana-git <aranaojavier@gmail.com>
2022-06-30 23:48:01 +0200

    Añadida estructura inicial para puppeteer

---
+++ loginTwitter.js
@@ -0,0 +1,24 @@
+
+/*
+* INICIA SESIÓN EN TWITTER
+*
+*
+*/
+
+const puppeteer = require("puppeteer");
+const prompt = require("prompt-sync")();
+const urlLogin = "https://twitter.com/i/flow/login";
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 10,
+        executablePath:"/usr/bin/google-chrome",
+    });
+const page = await browser.newPage();
+console.log(
+    "\n***************************** INICIO  TEST *****************************"
+  );
+await page.goto(urlLogin);
+
+});
