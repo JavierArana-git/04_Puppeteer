d288b8617fee4e7d26d7a6b246e5db6ef63314cb
JavierArana-git <aranaojavier@gmail.com>
2022-07-09 22:23:51 +0200

    Creación

--- loginTwitter.js
+++ loginTwitter.js
@@ -1,24 +1,21 @@
-
 /*
-* INICIA SESIÓN EN TWITTER
-*
-*
-*/
+ * INICIA SESIÓN EN TWITTER
+ *
+ *
+ */
 
 const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 const urlLogin = "https://twitter.com/i/flow/login";
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 10,
-        executablePath:"/usr/bin/google-chrome",
-    });
-const page = await browser.newPage();
-console.log(
-    "\n***************************** INICIO  TEST *****************************"
-  );
-await page.goto(urlLogin);
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 10,
+    executablePath:"/usr/bin/google-chrome",
+  });
+  const page = await browser.newPage();
+  console.log("\n***************************** INICIO  TEST *****************************");
+  await page.goto(urlLogin);
 
-});
+})();
