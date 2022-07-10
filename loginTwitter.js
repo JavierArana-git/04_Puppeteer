6c28e389d8d372289a4140278ddad51a8ad36935
JavierArana-git <aranaojavier@gmail.com>
2022-07-10 23:55:32 +0200

    Verificado username

--- loginTwitter.js
+++ loginTwitter.js
@@ -9,16 +9,12 @@
 const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 const urlLogin = "https://twitter.com/i/flow/login";
-const user = "ejemplo@gmail.com";
-const password = "123456";
 
 var user = prompt("User: ");
 var password = prompt("Password: ");
 (async () => {
   const browser = await puppeteer.launch({
-    args : [
-      '--window-size=1920,1080',
-    ],
+    args: ["--window-size=1920,1080"],
     headless: false,
     slowMo: 10,
     executablePath: "/usr/bin/google-chrome",
@@ -33,10 +29,10 @@ var password = prompt("Password: ");
   console.log("\n***************************** INICIO  TEST *****************************");
   await page.goto(urlLogin);
   delay(300);
-  await clickInSelector("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv", page,"Texto Usuario"); //Menú Artículo
-  await writeInput("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv",user,page,"",user,"Texto Usuario");
+  await clickInSelector("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv", page, "Texto Usuario"); //Menú Artículo
+  await writeInput("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input", user, page, "", user, "Texto Usuario");
 })();
-async function writeInput(selector, valueToWrite, page, checkLastValue, checkNewValue, alias="") {
+async function writeInput(selector, valueToWrite, page, checkLastValue, checkNewValue, alias = "") {
   try {
     element = await page.waitForSelector(selector, {
       timeout: 1500,
@@ -56,9 +52,9 @@ async function writeInput(selector, valueToWrite, page, checkLastValue, checkNew
       return input.value;
     });
     //Result Report
-    if (alias){
+    if (alias) {
       msg = `Cambio de valor en "${alias}": "${lastValue}" -> "${writtenValue}"`;
-    }else{
+    } else {
       msg = `Cambio de valor en "${selector}": "${lastValue}" -> "${writtenValue}"`;
     }
     //Check written value
@@ -68,9 +64,9 @@ async function writeInput(selector, valueToWrite, page, checkLastValue, checkNew
       msgLog(msg, false);
     }
   } catch (err) {
-    if (alias){
+    if (alias) {
       msg = `Selector "${alias}" no encontrado`;
-    }else{
+    } else {
       msg = `Selector "${selector}" no encontrado`;
     }
     msgLog(msg, false);
